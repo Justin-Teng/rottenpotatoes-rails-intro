bf6fde6968d36e649e90867d3313923615fcf07f
EC2 Default User <ec2-user@ip-172-31-18-243.us-east-2.compute.internal>
2019-10-03 05:00:00 +0000

    part 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,11 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if params.has_key? 'order_by'
+      @movies = Movie.order("#{params[:order_by]} asc")
+    else
+      @movies = Movie.all()
+    end
   end
 
   def new
